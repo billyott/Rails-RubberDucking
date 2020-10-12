b08e7f567bed29264f293d0908d5a9a11d84031b
billyott <willrichott@gmail.com>
2020-10-12 11:56:57 -0400

    Done.

--- app/controllers/ducks_controller.rb
+++ app/controllers/ducks_controller.rb
@@ -1,7 +1,44 @@
 class DucksController < ApplicationController
+  before_action :find_duck, only: [:show, :edit, :update, :destroy]
 
   def index
     @ducks = Duck.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @duck.update(duck_params)
+    redirect_to duck_path(@duck)
+  end
+
+  def new
+    @duck = Duck.new
+  end
+
+  def create
+    @duck = Duck.new(duck_params)
+    @duck.save
+    redirect_to duck_path(@duck)
+  end
+
+  def destroy
+    @employee.destroy
+    redirect_to employees_path
+  end
+
+  private
+
+  def find_duck
+    @duck = Duck.find(params[:id])
+  end
+
+  def duck_params
+    params.require(:duck).permit(:name, :description, :student_id)
   end
 
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,44 @@
 class StudentsController < ApplicationController
+  before_action :find_student, only: [:show, :edit, :update, :destroy]
 
   def index
     @students = Student.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @student.update(student_params)
+    redirect_to student_path(@student)
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(student_params)
+    @student.save
+    redirect_to student_path(@student)
+  end
+
+  def destroy
+    @employee.destroy
+    redirect_to employees_path
+  end
+
+  private
+
+  def find_student
+    @student = Student.find(params[:id])
+  end
+
+  def student_params
+    params.require(:student).permit(:name, :mod)
   end
 
 end
